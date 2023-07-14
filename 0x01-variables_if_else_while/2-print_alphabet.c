25f510d72a712cfe1132fa93a9d87f4c63eec47f
root <michaelg.email>
2023-07-14 16:00:52 -0700

    printing lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,9 @@
 int main (void)
 {
 char ch;
-for(ch = 'a', ch <= 'z', ch++)
-	{
-		putchar(ch);
-		putchar('\n');
-	}
+for(ch = 'a'; ch <= 'z'; ch++) {
+	putchar(ch);
+	putchar('\n');
+}
 return (0);
 }
