619390bd0b850acab9b78fe0f4d482afd4f9bf0c
root <michaelg.email>
2023-07-13 18:52:56 -0700

    size of data types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main block for printing sizeof dataTypes
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return (0);
+}
