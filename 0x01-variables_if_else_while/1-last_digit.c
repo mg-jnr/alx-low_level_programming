9a12f7b44641e1a97e195dab84b593dd1ee00cfc
root <michaelg.email>
2023-07-14 14:04:00 -0700

    finding the last digit of a random int

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	ld = n % 10
+	
+	if ( ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, ld);
+	}
+	else if(ld > 6 && ld !=0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n,ld
+		);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, ld);
+	}
+	
+	return (0);
+}
+
