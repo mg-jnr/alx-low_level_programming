2646d9c7ce231ed28124a48733222c7c843ae261
root <michaelg.email>
2023-07-14 19:47:57 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb > 10; comb++)
+	{
+		putchar(comb + '0');
+	}
+	for (comb = 9; comb>= 0; comb--)
+	{
+		putchar(comb + '0');
+	}
+putchar('\n');
+return (0);
+}
