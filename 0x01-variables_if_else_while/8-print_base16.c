1270c21eab7cb48c629d8c3920c1acd85ee9efb3
root <michaelg.email>
2023-07-14 19:16:47 -0700

    the hex work

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int hex;
+	for (hex = 0; hex <= 16; hex++)
+	{
+		if (hex < 10)
+		{
+			putchar (hex + '0')
+		}
+		else if ( hex >= 10)
+		{
+			putchar (hex + 87)
+		}
+	}
+putchar('\n');
+return (0);
+}
