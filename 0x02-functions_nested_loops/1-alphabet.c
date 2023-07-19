41cc48f44ea09162728b0fac48d21b7446ea55ad
root <michaelg.email>
2023-07-18 18:26:18 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Displays the alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	int i = 97;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
