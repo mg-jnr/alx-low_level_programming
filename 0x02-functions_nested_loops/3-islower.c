1ea117e559e13b60d58014539f67cb9d57c77c59
root <michaelg.email>
2023-07-18 19:14:49 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase letter
- *@c - The character to be checked
+ *@c: The character to be checked
  * Return: 1 for lowercase character or ) for anything else
  */
 int _islower(int c)
