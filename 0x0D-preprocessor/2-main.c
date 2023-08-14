a154b1d0b8f176a74897e9adb9200f3773579b65
root <michaelg.email>
2023-08-14 14:12:33 -0700

    Preprocessor Done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Prints the  name of the fileof the program
+  *
+  * Return: Always zero, regardless
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
