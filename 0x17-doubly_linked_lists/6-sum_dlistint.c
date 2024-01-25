e426d76daea3b118474acac940f0a6bd7e731e79
mg.jnr <michaelgozo4@gmail.com>
2024-01-25 05:33:46 -0800

    Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,20 +8,19 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-        dlistint_t *current;
-        int sum = 0;
+	dlistint_t *current;
+	int sum = 0;
 
-        if (head == NULL)
-        {
-                return (0);
-        }
+	if (head == NULL)
+	{
+		return (0);
+	}
 
-        current = head;
-        while (current)
-        {
-                sum = sum + current->n;
-                current = current->next;
-        }
-        return (sum);
-}
-~              
+	current = head;
+	while (current)
+	{
+		sum = sum + current->n;
+		current = current->next;
+	}
+	return (sum);
+} 
