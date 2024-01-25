abad17d62c84d7efb7ce06f495ede21fab75c03e
mg.jnr <michaelgozo4@gmail.com>
2024-01-25 05:26:11 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        const dlistint_t *current = h;
-        int i = 0;
+	const dlistint_t *current = h;
+	int i = 0;
 
-        while (current != NULL)
-        {
-                printf("%d\n", current->n);
-                i++;
-                current = current->next;
-        }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		i++;
+		current = current->next;
+	}
 
-        return (i);
+	return (i);
 }
